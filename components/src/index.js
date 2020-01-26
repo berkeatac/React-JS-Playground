ef5be381ef279c64ba394823e7ea07c4e7b0f816
berkeatac <berkeatac@sabanciuniv.edu>
2020-01-26 06:44:42 +0300

    nested components

--- components/src/index.js
+++ components/src/index.js
@@ -1,24 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
-//We can duplicate this component
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
 const App = () => {
   return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+    <div className="ui container comments" style={{ marginTop: 20 }}>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 2am"
+          comment="Yo"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 3am"
+          comment="Hey"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 2am"
+          comment="hello"
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
