6981d644ddd1151ea0e00f20d8dd97f16248219a
berkeatac <berkeatac@sabanciuniv.edu>
2020-01-28 16:05:10 +0300

    class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi There</div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      pos => console.log(pos),
+      err => console.log(err)
+    );
+
+    return <h1>Latitude: </h1>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
