83aa7d8866707a4f54ce3caf187a388919d3dd1a
berkeatac <berkeatac@sabanciuniv.edu>
2020-01-31 04:37:38 +0300

    axios with async/await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization:
+          "Client-ID 5824724607d2fad613cba2c83a8021f936843a005cb1c4979b647b52bfaafdda"
+      },
+      params: {
+        query: term
+      }
+    });
+    console.log(res.data.results);
   }
 
   render() {
