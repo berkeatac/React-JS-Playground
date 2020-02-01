3cdfef2599d8b439fb7a11038785317918be90cb
berkeatac <berkeatac@sabanciuniv.edu>
2020-02-01 23:39:54 +0300

    React Ref attributed to component

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef;
+  }
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img src={urls.regular} alt={description} />
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
