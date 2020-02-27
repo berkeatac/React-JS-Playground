77a5cbc8ce38c692b2ac46d25aea3a88c0209d24
berkeatac <berkeatac@sabanciuniv.edu>
2020-02-27 23:59:36 +0300

    routing

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function StreamEdit() {
+const StreamEdit = props => {
+  console.log(props);
+
   return <div>StreamEdit</div>;
-}
+};
+
+export default StreamEdit;
