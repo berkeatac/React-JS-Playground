7781d0b15b41055d066f8eed1880129e2b197cc8
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-03 00:41:48 +0300

    created reusable form component

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = props => {
-  // console.log(props);
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-  return <div>StreamEdit</div>;
+  render() {
+    console.log(this.props.stream);
+    const stream = this.props.stream;
+    return <div>{stream ? stream.title : ""}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
