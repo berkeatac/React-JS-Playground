7818cfd1a219f93682a87d0bb4af75b4ccc0e3a2
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-05 02:04:32 +0300

    implemented context consumer for button

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,10 +2,17 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext;
 
   render() {
-    return <button className="ui button primary">Submit</button>;
+    // const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
