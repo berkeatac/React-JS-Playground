fc745f0d6f5b81f750d288aab450d4a7e87c521f
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-06 02:36:57 +0300

    Used hooks for resource fetch and reusablity

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,22 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
+import UserList from "./UserList";
 
-class App extends React.Component {
-  state = { resource: "posts" };
-
-  render() {
-    return (
-      <div className="ui container">
-        <div>
-          <button onClick={() => this.setState({ resource: "posts" })}>
-            Posts
-          </button>
-          <button onClick={() => this.setState({ resource: "todos" })}>
-            Todos
-          </button>
-        </div>
-        {this.state.resource}
+const App = () => {
+  const [resource, setResource] = useState("posts");
+  return (
+    <div className="ui container">
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
       </div>
-    );
-  }
-}
+      <ResourceList resource={resource} />
+      <UserList></UserList>
+    </div>
+  );
+};
 export default App;
