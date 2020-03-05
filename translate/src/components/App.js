5c6fb1c1173f8a973a871a6df5da0a3755605737
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-06 00:07:02 +0300

    added selector to main app

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -13,12 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <div>Select a language:</div>
-        <i
-          className="flag us"
-          onClick={() => this.onLanguageChange("english")}
-        />
-        <i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
         {this.state.language}
         <LanguageContext.Provider value={this.state.language}>
           <ColorContext.Provider value={"red"}>
