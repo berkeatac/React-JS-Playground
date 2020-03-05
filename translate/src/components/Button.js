3240fe3c2c441e48916831483a3b977e7c459b46
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-05 23:18:11 +0300

    Added 2 context providers to single component

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,17 +1,26 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   // static contextType = LanguageContext;
 
-  render() {
-    // const text = this.context === "english" ? "Submit" : "Voorleggen";
+  renderButton(value) {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${value}`}>
         <LanguageContext.Consumer>
           {value => (value === "english" ? "Submit" : "Voorleggen")}
         </LanguageContext.Consumer>
       </button>
+    );
+  }
+
+  render() {
+    // const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return (
+      <ColorContext.Consumer>
+        {value => this.renderButton(value)}
+      </ColorContext.Consumer>
     );
   }
 }
